a9f5fc82aa913b28446454d122d468467c622b98
koppthe <kopptsoft@gmail.com>
2020-05-09 11:36:36 +0800

    fix(template): fix object-c template error

---
+++ test/JDIF_Iconfont.h
@@ -0,0 +1,18 @@
+
+  //
+  // JDIF_Iconfont.h
+  // fontFamily:iconfont
+  //
+  // Version 1.0
+  // Copyright (C) 2019 by original authors @ master Gao
+  //
+
+  #ifndef JDIF_Iconfont_H
+  #define JDIF_Iconfont_H
+
+  NSString * const JDIF_Iconfont = @"iconfont";
+
+  NSString * const JDIF_ICON_7DAYS = @"\U0000e975"; // icon_7days1
+  NSString * const JDIF_ICON_HOUR = @"\U0000e973"; // icon_7days1
+
+  #endif
---
+++ test/icon_iconfont.js
@@ -0,0 +1,4 @@
+export default {
+  'icon_7days': 59765,// icon_7days1
+  'icon_hour': 59763,// icon_7days1
+}
